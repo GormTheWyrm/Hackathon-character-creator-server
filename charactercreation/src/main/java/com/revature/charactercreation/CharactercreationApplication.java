c80d7ea5fde412aec45d3fc4cb6d466f0f469762
AnderTwelve <willdabeastlol@gmail.com>
2021-07-22 10:37:05 -0500

    initial commit

---
+++ charactercreation/src/main/java/com/revature/charactercreation/CharactercreationApplication.java
@@ -0,0 +1,13 @@
+package com.revature.charactercreation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharactercreationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharactercreationApplication.class, args);
+	}
+
+}
---
+++ charactercreation/src/test/java/com/revature/charactercreation/CharactercreationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.charactercreation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharactercreationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
